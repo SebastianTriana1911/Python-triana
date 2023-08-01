133d9fdfcca02e637f067a4e9ae90a91e0c4dac1
Sebastian <sebastiantriana1911@gmail.com>
2023-08-01 00:11:46 -0500

    Archivos y Laboratorio

--- Ejercicios_instructor/Ejercicios Archivos/Ejercicio1.py
+++ Ejercicios_instructor/Ejercicios Archivos/Ejercicio1.py
@@ -1,52 +1,74 @@
-def crearArchivo (nomArchivo):
-    try:
-        archivo = open (f"C:\\Triana\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "x")
-        archivo.close ()
-    except FileExistsError:
-        print ("El archivo ya existe")
-        
-def ingresarInfo (nomArchivo):
-    try:
-        archivo = open (f"C:\\Triana\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "a")
-        lineas = int(input ("Ingrese cuantos saltos de linea desea realizar: "))
-        for i in range (lineas + 1):
-            texto = input ("Ingrese lo que desea añadir en el texto: ")
-            archivo.write (f"{texto} \n")
-            print ("\nLo que ingreso fue adjuntado al archivo exitosamente")
-            archivo.close()
-    except:
-            print ("El nombre del archivo es incorrecto")
-                
 while True:
     print ("""\nOpciones que puede realiza\n
     1. Crear un archivo
     2. Ingresar informacion a un archivo
     3. Ver cuantas lineas contiene un archivo
-    4. Salir
+    4. Leer informacion que contenga un archivo
+    5. Salir
     """)
     opcion = int (input("Ingrese el numero de la opcion que desea realizar: "))
     
     if opcion == 1:
+
+        def crearArchivo (nomArchivo):
+            try:
+                archivo = open (f"C:\\Users\\Sebastian\\OneDrive\\Escritorio\\Clone\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "x")
+                print ("El archivo se creo exitosamente")
+                archivo.close ()
+            except FileExistsError:
+                print ("El archivo ya existe")
+
         archivo = input("Ingrese el nombre del archivo: ")
         crearArchivo(archivo)
 
     if opcion == 2:
-        archivo = input ("Ingrese el nombre del archivo donde va a ingresar informacion: ")
-        ingresarInfo(archivo)
+
+        def ingresarInfo (nomArchivo):
+            try:
+                archivo2 = open (f"C:\\Users\\Sebastian\\OneDrive\\Escritorio\\Clone\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "a")
+                cont = 0
+                lineas = int(input ("Ingrese cuantos saltos de linea desea realizar: "))
+                for i in range (lineas):
+                    cont = cont + 1
+                    texto = input (f"Ingrese lo que desea añadir en la linea {cont}: ")
+                    archivo2.write (f"{texto} \n")
+                print ("\nLo que ingreso fue adjuntado al archivo exitosamente")
+                archivo2.close()
+            except:
+                print ("Error en la ejecucion del codigo")
+
+        archivo2 = input ("Ingrese el nombre del archivo donde va a ingresar informacion: ")
+        ingresarInfo(archivo2)
         
     if opcion == 3:
+
         def lineasExistentes (nomArchivo):
             try:
-                archivo = open (nomArchivo, "rt")
+                archivo = open (f"C:\\Users\\Sebastian\\OneDrive\\Escritorio\\Clone\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "r")
                 cont = 0
                 for i in archivo.readlines():
                     cont = cont + 1
-                print (f"La cantidad de lineas que hay en el texto es de {cont}")
+                print (f"La cantidad de lineas que hay en el texto es de {cont} lineas")
                 archivo.close()
             except:
-                print ("...")
-        archivo = input ("Ingrese el nombre del archivo: ")
-        lineasExistentes (archivo)
+                print ("Error en la ejecucion del codigo")
+
+        archivo3 = input ("Ingrese el nombre del archivo: ")
+        lineasExistentes (archivo3)
         
     if opcion == 4:
+
+        def leerArchivo (nomArchivo):
+            try:
+                archivo = open (f"C:\\Users\\Sebastian\\OneDrive\\Escritorio\\Clone\\pythontriana\\Ejercicios_instructor\\Ejercicios Archivos\\{nomArchivo}", "r")
+                for i in archivo:
+                    print (i, end = "")
+            except FileNotFoundError:
+                print ("No existe ningun archivo con ese nombre")
+
+        archivo4 = input ("Ingrese el nombre del archivo: ")
+        leerArchivo (archivo4)
+
+    if opcion == 5:
+        print ("\nHASTA PRONTO\n")
         break
--- Ejercicios_instructor/Laboratorio/laboratorio.py
+++ Ejercicios_instructor/Laboratorio/laboratorio.py
@@ -1,25 +1,19 @@
-abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+lista = []
 diccionario = {}
-diccionario2 = {}
-try:
-    # archivo = input ("Ingrese la direccion del archivo de entrada: ")
-    archivo2 = open ("C:\\Users\\Sebastian\\OneDrive\\Escritorio\\Sena\\Instructor Sumuel\\laboratorio.txt", 'rt')
-    for key in abecedario:
-        diccionario [key] = " "
-    for i in archivo2:
-        cont = 0
-        for a in i:
-            letra = a.lower()
-            for clave in diccionario:
-                if letra == clave:
-                    print (clave)
-                    print (f"a {letra}")
-                    cont = cont + 1
-                    valor = cont
-                    diccionario2 [clave] = valor
-        for clave,valor in diccionario2.items():
-            print (clave,"->",valor)
-    archivo2.close ()
-except:
-    print ("A surgido un error")
+archi = input ("Ingrese la ruta de su archivo: ")
+archivo = open (archi, "r")
+
+for texto in archivo:
+    for letras in texto:
+        l = letras.lower()
+        lista.append (l)
+lista.sort()
+for letra in lista:
+    if letra not in diccionario:
+        diccionario [letra] = 1
+    else:
+        diccionario [letra] = diccionario [letra] + 1
+key = " "
+diccionario.pop (key, None)
+for clave, valor in diccionario.items():
+    print (f"{clave} -> {valor}")
